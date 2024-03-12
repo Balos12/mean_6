899b5ce99e11dd3b05666846693d2798c056e7e5
Balos12 <102786215+Balos12@users.noreply.github.com>
2024-03-12 23:30:19 +0500

    Add files via upload

---
+++ server.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const multer = require('multer');
+const AWS = require('aws-sdk');
+const fs = require('fs');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+const upload = multer({ dest: 'uploads/' });
+
+const s3 = new AWS.S3({
+    accessKeyId: 'AKIAZI2LIIIPH7WRBUWU',
+    secretAccessKey: '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',
+});
+
+const bucket_name = 'itsmemario';
+console.log("Bucket name:", bucket_name);
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.post('/upload', upload.array('files'), async (req, res) => {
+    try {
+      const promises = [];
+      for (const file of req.files) {
+        const fileStream = fs.createReadStream(file.path);
+        const params = {
+          Bucket: bucket_name,
+          Key: file.originalname,
+          Body: fileStream
+        };
+        const uploadPromise = s3.upload(params).promise();
+        promises.push(uploadPromise);
+      }
+      const results = await Promise.all(promises);
+      req.files.forEach(file => fs.unlinkSync(file.path));
+      res.send('Files uploaded successfully');
+    } catch (error) {
+      res.status(500).send('Error uploading files: ' + error.message);
+    }
+  });
+  
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
